5f2c89820f60880b569ba3ca8188e9f7eef0b338
marc7soto-web <marc7soto@gmail.com>
2026-08-23 18:47:22 -0300

    Se implementa modulo de eliminacion de libros

--- BlazorBiblioteca/Controllers/LibrosController.cs
+++ BlazorBiblioteca/Controllers/LibrosController.cs
@@ -67,5 +67,23 @@ namespace BlazorBiblioteca.Controllers
 
             return NoContent();
         }
+
+        // Elimina un libro de la base de datos.
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLibro(int id)
+        {
+           var libro = await _context.Libros.FindAsync(id);
+
+           if (libro == null)
+            {
+                return NotFound();
+            }
+
+            _context.Libros.Remove(libro);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
